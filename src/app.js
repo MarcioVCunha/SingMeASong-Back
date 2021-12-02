47f38ba7c9da43e8d10bb9c2934c3c267dcbb52e
Marcio <marcioabcd11@gmail.com>
2021-12-02 07:54:25 -0300

    downvote done, delete when -5 votes done

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import cors from 'cors';
 import express from 'express';
 import * as postMusicController from './controllers/postMusic.js';
 import * as upvoteController from './controllers/upvoteMusic.js';
+import * as downvoteController from './controllers/downvoteMusc.js';
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 
 app.post('/recommendations', postMusicController.postMusic);
 app.post('/recommendations/:id/upvote', upvoteController.upvote);
+app.post('/recommendations/:id/downvote', downvoteController.downvote);
 
 export default app;
--- src/services/upvote.js
+++ src/services/upvote.js
@@ -3,7 +3,7 @@ import * as musicRepository from '../repositories/music.js';
 const upvote = async (id) => {
   const music = await musicRepository.selectSingleMusicById(id);
 
-  console.log(music);
+  if (!music) return (404);
 
   const upvoteAdded = await musicRepository.upvote(id, music.votes);
 
