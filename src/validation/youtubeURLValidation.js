0dafff45c98cc0ab6332054ae497fcef225eba8f
Matheus Galdino <matheus@respondeai.com.br>
2021-12-07 12:49:38 -0300

    preparando o review

--- src/validation/youtubeURLValidation.js
+++
@@ -1,9 +0,0 @@
-const validateYoutubeURL = (link) => {
-  const rule = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
-  if (link.match(rule)) {
-    return true;
-  }
-  return false;
-};
-
-export { validateYoutubeURL };
